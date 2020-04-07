529b0f82fe5bc3d249b140a0403ac95afe1575ad
Benjamin Chariou <chariou.benjamin@gmail.com>
2020-04-07 16:13:42 +0200

    Voilà les gestions des vidéos

---
+++ HAWAF/Assets/Script/Plaque.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plaque : MonoBehaviour
+{
+    public VideoManager videoManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Video");
+            videoManager.StartVideo();
+        }
+    }
+}
---
+++ HAWAF/Assets/Script/VideoManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoManager : MonoBehaviour
+{
+    public UnityEngine.Video.VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartVideo()
+    {
+        videoPlayer.Play();
+    }
+
+    public void StopVideo()
+    {
+        videoPlayer.Stop();
+    }
+}
