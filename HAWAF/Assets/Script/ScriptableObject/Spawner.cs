6d5ab7b1a922639f9fb0e69cfa307e14fc665c71
W3R10S <47816763+W3R10S@users.noreply.github.com>
2020-04-07 14:53:01 +0200

    Camera first person + glowing object + cursor + my version of the characterController

---
+++ HAWAF/Assets/Script/ScriptableObject/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public SpawnManagerScriptableObject spawnManagerValues;
+    int instanceNumber = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEntities();
+    }
+
+    void SpawnEntities()
+    {
+        int currentSpawnPointIndex = 0;
+
+        for (int i = 0; i < spawnManagerValues.numberOfPrefabsToCreate; i++)
+        {
+            // Creates an instance of the prefab at the current spawn point.
+            GameObject currentEntity = Instantiate(spawnManagerValues.Prefabs[currentSpawnPointIndex], spawnManagerValues.spawnPoints[currentSpawnPointIndex], Quaternion.identity);
+
+            // Sets the name of the instantiated entity to be the string defined in the ScriptableObject and then appends it with a unique number. 
+            currentEntity.name = spawnManagerValues.prefabName + instanceNumber;
+
+            // Moves to the next spawn point index. If it goes out of range, it wraps back to the start.
+            currentSpawnPointIndex = (currentSpawnPointIndex + 1) % spawnManagerValues.spawnPoints.Length;
+
+            instanceNumber++;
+        }
+    }
+}
