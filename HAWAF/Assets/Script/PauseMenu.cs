2a58e148d90f8283325cbcae4c02f22c058b1394
Benjamin Chariou <chariou.benjamin@gmail.com>
2020-04-09 11:32:43 +0200

    Menus

---
+++ HAWAF/Assets/Script/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool isPaused = false;
+
+    public GameObject pauseMenu;
+
+    public int menuId;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(menuId);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
