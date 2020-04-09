406c5a8748dfc2a600039a7d73958c6b3beaa570
Benjamin Chariou <chariou.benjamin@gmail.com>
2020-04-09 16:35:28 +0200

    Menus + Condition Défaite

---
+++ HAWAF/Assets/Script/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public Text timerText;
+    private float startTime;
+    public int desiredTimeInSeconds;
+    public MainMenu loadingManager;
+    public int gameOverSceneID;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time - startTime;
+
+        string minutes =  ((int)(desiredTimeInSeconds - t) / 60).ToString();
+        string seconds = (((desiredTimeInSeconds)-t) % 60).ToString("f0");
+
+        float minutesLeft = ((float)desiredTimeInSeconds - t)/60;
+
+        timerText.text = minutes + " minutes " + seconds + " restantes";
+
+        if(minutesLeft < 0)
+        {
+            loadingManager.LoadScene(gameOverSceneID);
+        }
+    }
+}
